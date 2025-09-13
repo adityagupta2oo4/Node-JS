690652befafe231fd35789b22cee5e92e02e7e75
adityagupta2oo4 <ag751000@gmail.com>
2025-09-13 10:28:29 +0000

    hello world

---
+++ Hello-World/hello.js
@@ -0,0 +1,15 @@
+console.log("hello there! I am running using node js runtime environment");
+
+// console.log(alert("Hey")) // this will give us an error
+
+//why? we know that v8 engine
+//  was embedded with the C++ 
+// that's how we got node js runtime environment
+// we have to run js on server using node js so everything related to UI / DOm is 
+// removed from the node js
+
+//npm- node package manager 
+// whenever we create a new we have to npm init for the template(package.json)
+// package.json - configuration file (this is uses to write different script install external dependencies)
+
+//npm run start  -> will automatically run the hello.js as we have edited in the json file
