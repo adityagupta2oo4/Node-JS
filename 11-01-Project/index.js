647bb171642366f70bdc00f42d51a3404a5ff8b8
adityagupta2oo4 <ag751000@gmail.com>
2025-11-05 17:49:27 +0000

    created REST API

---
+++ 11-01-Project/index.js
@@ -0,0 +1,74 @@
+
+const express = require("express");
+const app = express();
+const PORT = 5170;
+//acquire data
+const users = require("./MOCK_DATA.json");
+const fs = require('fs');
+
+//aplying middleware - plugin
+app.use(express.urlencoded({extended:false})); //parses traditional form submissions
+
+
+//routes
+
+//example of hybrid server -- 
+// ssr
+app.get("/users", (req, res) => {
+    const html = `
+    <ul>
+    ${users.map((user) => {
+        return `<li>${user.first_name}</li>`
+    }).join("")
+        }
+    </ul>
+    `;
+    res.send(html);
+})
+//csr
+app.get("/api/users", (req, res) => {
+
+    return res.json(users);
+})
+
+// let's do others
+
+app.post('/api/users', (req, res) => {
+    //to do create new user
+    const body = req.body; // whatever request we send from backend is available in the body
+    console.log('Body',body);
+
+    users.push({id: users.length + 1,...body}) // to append but we also have to write in file for that we need fs model
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users),(err, data)=>{
+        return res.json({ status: "succes",id :users.length + 1});
+    });
+});
+
+
+// other way if route is same in multiple
+
+app
+    .route('/api/users/:id')
+    .get((req, res) => {
+        const id = Number(req.params.id); // by deafult id from req will in string
+        const user = users.find((user) => {
+            if (user.id === id) return user;
+        })
+        return res.json(user);
+    })
+    .patch((req, res) => {
+        //to do edit user with id
+        return res.json({ status: "pending" });
+    })
+    .delete((req, res) => {
+        //to do delete user with id
+        return res.json({ status: "pending" });
+    })
+
+
+
+
+
+//server started
+
+app.listen(PORT, () => console.log(`Server started at port : ${PORT}`));
