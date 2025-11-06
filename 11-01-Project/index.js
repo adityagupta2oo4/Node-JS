7c29bb9c6b533aa94fa62d8323934535b4d2adc1
adityagupta2oo4 <ag751000@gmail.com>
2025-11-06 06:09:22 +0000

    middleware code

--- 11-01-Project/index.js
+++ 11-01-Project/index.js
@@ -7,9 +7,34 @@ const users = require("./MOCK_DATA.json");
 const fs = require('fs');
 
 //aplying middleware - plugin
-app.use(express.urlencoded({extended:false})); //parses traditional form submissions
+app.use(express.urlencoded({extended:false})); // convert form data into object and then store it in body which is in req
 
 
+app.use((req,res,next) =>{
+    console.log("Helloo from middleware 1");
+    // return res.json({status : "in middleware"}) // i ended the response here so it didn't wnet to next
+
+    //practical use case
+
+    fs.appendFile('log.txt',`\n ${Date.now()} ${req.ip} ${req.method} ${req.path}`,
+        (err,data)=>{
+            next();
+        }
+    );
+
+})
+
+app.use((req,res,next) =>{
+    console.log("Helloo from middleware 2");
+    req.myUserName = "aditya"; //changing the request and it can be accessed by other routes and middleware
+    next(); 
+})
+
+app.use((req,res,next) =>{
+    console.log("Helloo from middleware 3");
+    console.log("hello ",req.myUserName);
+    next(); 
+})
 //routes
 
 //example of hybrid server -- 
@@ -40,7 +65,7 @@ app.post('/api/users', (req, res) => {
 
     users.push({id: users.length + 1,...body}) // to append but we also have to write in file for that we need fs model
     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users),(err, data)=>{
-        return res.json({ status: "succes",id :users.length + 1});
+        return res.json({ status: "succes",id :users.length});
     });
 });
 
@@ -58,11 +83,29 @@ app
     })
     .patch((req, res) => {
         //to do edit user with id
-        return res.json({ status: "pending" });
+
+        const id = Number(req.params.id);
+        const body = req.body;
+        users.map((user)=>{
+            if(user.id === id ){
+                user.email = body.email;
+            }
+        })
+
+        fs.writeFile('./MOCK_DATA.json',JSON.stringify(users), (err,data)=>{
+            return res.json({status : "Success"});
+
+        })
+
     })
     .delete((req, res) => {
         //to do delete user with id
-        return res.json({ status: "pending" });
+        const id = Number(req.params.id);
+
+        const data = users.filter(user => user.id !== id);
+        fs.writeFile('./MOCK_DATA.json',JSON.stringify(data),(err,data)=>{
+            return res.json({status : "success"});
+        })
     })
 
 
